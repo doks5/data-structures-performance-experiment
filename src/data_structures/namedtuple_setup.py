8d26d7e64cd674b27ab24f219e69856cf538f0c0
Andon Andonov <andon_b_andonov@proton.me>
2024-10-28 15:36:27 +0200

    Clean up namedtuple test setup
    This change updates **namedtuple_setup.py** to clean up unnecessary code and streamline the overall setup.
    
    Signed-off-by: Andon Andonov <andon_b_andonov@proton.me>

--- src/data_structures/namedtuple_setup.py
+++ src/data_structures/namedtuple_setup.py
@@ -4,25 +4,14 @@ from typing import NamedTuple
 
 BasicNamedTuple = namedtuple("BasicNamedTuple", f"{' '.join([f'key{i}' for i in range(1, 26)])}")
 
-
-def generate_and_call_basic_namedtuple(iteration: int):
-    instance_i = BasicNamedTuple(*[f"val_{iteration}__{i}" for i in range(1, 26)])
-
-    instance_i.key3
-    instance_i.key8
-    instance_i.key15
-    instance_i.key19
-    instance_i.key25
+TypedNamedTuple = NamedTuple("TypedNamedTuple", [(f'key{i}', str) for i in range(1, 26)])
 
 
-TypedNamedTuple = NamedTuple("TypedNamedTuple", [(f'key{field}', str) for field in range(1, 26)])
+def access_namedtuple_elements(data_struct: tuple):
+    data_struct.key5
+    data_struct.key15
+    data_struct.key25
 
-
-def generate_and_call_typed_namedtuple(iteration: int):
-    instance_i = TypedNamedTuple(*[f"val_{iteration}__{i}" for i in range(1, 26)])
-
-    instance_i.key5
-    instance_i.key11
-    instance_i.key18
-    instance_i.key21
-    instance_i.key25
+    "key12" in data_struct._fields
+    "nonexistent_field" in data_struct._fields
+    "nonexistent_value" in data_struct
