072b98794e51a3e7d1e67d9637a85e17980d20cb
Andon Andonov <andon_b_andonov@proton.me>
2024-11-21 16:49:04 +0200

    Update dictionary setup
    This change updates the dictionary test setup to be more streamlined and similar to the setups
    the other data structures.
    
    Signed-off-by: Andon Andonov <andon_b_andonov@proton.me>

--- src/data_structures/dict_setup.py
+++ src/data_structures/dict_setup.py
@@ -6,10 +6,9 @@ class TestTypedDict(TypedDict):
 
 
 def access_dict_elements(data_struct: dict) -> None:
-    data_struct["key12"]
-    data_struct["key1"]
+    data_struct.get("key12")
+    data_struct.get("key1")
     data_struct.get("key21")
 
     "key15" in data_struct.keys()
     "nonexistent_key" in data_struct.keys()
-    "nonexistent_value" in data_struct.values()
