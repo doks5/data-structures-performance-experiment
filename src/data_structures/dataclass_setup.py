a4b26370bfac51cd8acfce46d9e1031ffd1d350b
Andon Andonov <andon_b_andonov@proton.me>
2024-11-21 22:52:33 +0200

    Fix typo in dataclass setup
    This change fixes typos in **dataclass_setup.py**, where the name of the basic dataclass class was
    not replaced in all places where it is used.
    
    Signed-off-by: Andon Andonov <andon_b_andonov@proton.me>

--- src/data_structures/dataclass_setup.py
+++ src/data_structures/dataclass_setup.py
@@ -22,11 +22,11 @@ SlottedDataclass = make_dataclass(
 )
 
 
-def field_present(data_class: TestDataclass | ImmutableDataclass, field_name: str) -> bool:
+def field_present(data_class: BasicDataclass | ImmutableDataclass, field_name: str) -> bool:
     return any(field.name == field_name for field in fields(data_class))
 
 
-def access_dataclass_elements(data_struct: TestDataclass | ImmutableDataclass) -> None:
+def access_dataclass_elements(data_struct: BasicDataclass | ImmutableDataclass) -> None:
     data_struct.key1
     data_struct.key15
     data_struct.key25
