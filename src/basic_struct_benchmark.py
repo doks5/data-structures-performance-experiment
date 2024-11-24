21824c136c2a31960580c9e2b61405059ae300d4
Andon Andonov <andon_b_andonov@proton.me>
2024-11-24 22:52:18 +0200

    Add typed dict to basic_struct_benchmark
    This change updates **basic_struct_benchmark.py** to also test the "BasicTypedDict" typed dict.
    
    Signed-off-by: Andon Andonov <andon_b_andonov@proton.me>

--- src/basic_struct_benchmark.py
+++ src/basic_struct_benchmark.py
@@ -1,5 +1,6 @@
 from data_structures.namedtuple_setup import BasicNamedTuple
 from data_structures.namedtuple_setup import access_namedtuple_elements
+from data_structures.dict_setup import BasicTypedDict
 from data_structures.dict_setup import access_dict_elements
 from data_structures.dataclass_setup import BasicDataclass
 from data_structures.dataclass_setup import access_dataclass_elements
@@ -12,31 +13,36 @@ import utils as ut
 def main():
     namedtuple_measurements = []
     dict_measurements = []
+    basic_typed_dict_measurements = []
     basic_dataclass_measurements = []
     pydantic_measurements = []
 
     for i in range(1_000_000):
         namedtuple_measurements.append(ut.benchmark_namedtuple(i, BasicNamedTuple, access_namedtuple_elements))
         dict_measurements.append(ut.benchmark_dict(i, access_dict_elements))
+        basic_typed_dict_measurements.append(ut.benchmark_typeddict(i, BasicTypedDict, access_dict_elements))
         basic_dataclass_measurements.append(ut.benchmark_dataclasses(i, BasicDataclass, access_dataclass_elements))
         pydantic_measurements.append(ut.benchmark_pydantic(i, TestPydanticModel, access_pydantic_elements))
 
     namedtuple_avg_size, namedtuple_avg_time = ut.calculate_averages(namedtuple_measurements)
     dict_avg_size, dict_avg_time = ut.calculate_averages(dict_measurements)
+    typed_dict_avg_size, typed_dict_avg_time = ut.calculate_averages(basic_typed_dict_measurements)
     basic_dataclass_avg_size, basic_dataclass_avg_time = ut.calculate_averages(basic_dataclass_measurements)
     pydantic_avg_size, pydantic_avg_time = ut.calculate_averages(pydantic_measurements)
 
     average_sizes = {
-        "namedtuple": namedtuple_avg_size,
-        "dictionary": dict_avg_size,
-        "dataclass": basic_dataclass_avg_size,
-        "pydantic": pydantic_avg_size
+        "NamedTuple": namedtuple_avg_size,
+        "Dictionary": dict_avg_size,
+        "TypedDict": typed_dict_avg_size,
+        "Dataclass": basic_dataclass_avg_size,
+        "Pydantic": pydantic_avg_size
     }
     average_times = {
-        "namedtuple": namedtuple_avg_time,
-        "dictionary": dict_avg_time,
-        "dataclass": basic_dataclass_avg_time,
-        "pydantic": pydantic_avg_time
+        "NamedTuple": namedtuple_avg_time,
+        "Dictionary": dict_avg_time,
+        "TypedDict": typed_dict_avg_time,
+        "Dataclass": basic_dataclass_avg_time,
+        "Pydantic": pydantic_avg_time
     }
 
     ut.generate_diagram(
