85d5b2e2e5e9da8d80a7393098944c6519683a95
Andon Andonov <andon_b_andonov@proton.me>
2024-11-29 23:21:57 +0200

    Add CachelessPydanticModel to pydantic_setup.py
    This change adds a **CachelessPydanticModel** model to pydantic_setup.py, which is used in performance tests.
    
    Signed-off-by: Andon Andonov <andon_b_andonov@proton.me>

--- src/data_structures/pydantic_setup.py
+++ src/data_structures/pydantic_setup.py
@@ -1,10 +1,14 @@
-from pydantic import create_model
+from pydantic import create_model, ConfigDict
 
 
 fields: dict = {f"key{i}": (str, None) for i in range(1, 26)}
 
 
 BasicPydanticModel = create_model('BasicPydanticModel', **fields)
+
+CachelessPydanticModel = create_model(
+'BasicPydanticModel', __config__=ConfigDict(cache_strings='none'), **fields
+)
 
 
 def access_pydantic_elements(data_struct: BasicPydanticModel):
